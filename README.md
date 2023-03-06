37676f3d8a3a4b7536143eb5fed865b076f2f735
Nathan Laan <nathan@laan.pro>
2023-03-05 22:42:54 -0700

    feat: v0.1.0 tab list functionality.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tab-manager
-Google Chrome extension for managing tabs.
+# Tab-Manager
+
+Tab-Manager is a Google Chrome extension for managing and searching through the list of open tabs.
+
+## Roadmap
+
+- [ ] v0.1.0
+  - [ ] Display list of all tabs.
+  - [ ] List shows favicon and title.
+  - [ ] Clicking item in tab list sends focus to that tab, and closes the extension window.
+- [ ] v0.1.1
+  - [ ] The search input allows the user to filter the list of tabs.
+
+## References
+
+- Icons by [Flaticon](https://www.flaticon.com/free-icon/letter-t_3666228)
---
+++ src/popup.js
@@ -0,0 +1,51 @@
+var listElement;
+
+function onLoadTabs(tabs) {
+  for (const tab of tabs) {
+    listElement.appendChild(createTabElement(tab));
+  }
+}
+
+function onLoadTabsError(error) {
+  console.error(`Error: ${error}`);
+}
+
+window.onload = function () {
+  listElement = document.getElementById('tablist');
+
+  chrome.tabs.query({})
+    .then(onLoadTabs, onLoadTabsError);
+
+  const searchElement = document.getElementById('tabSearch');
+  searchElement.addEventListener('input', function(e) {
+    // GET FILTERED
+  });
+  searchElement.focus();
+};
+
+function createTabElement(tab) {
+  const tabElement = document.createElement('div');
+  tabElement.className = "tab-item";
+  const tabIconElement = document.createElement("img");
+  tabIconElement.className = "tab-item-icon";
+  tabIconElement.src = tab.favIconUrl;
+  const tabTitleElement = document.createElement('div');
+  tabTitleElement.className = "tab-item-title";
+  tabTitleElement.innerText = tab.title;
+  tabTitleElement.onclick = function () {
+    try {
+      chrome.windows.update(tab.windowId, { focused: true });
+      chrome.tabs.update(tab.id, { active: true });
+      window.close();
+    } catch (e) {
+      // TODO: User error message?
+      console.error(e);
+    }
+  }
+  tabElement.appendChild(tabIconElement);
+  tabElement.appendChild(tabTitleElement);
+  //
+  // TODO: Add close button.
+  //
+  return tabElement;
+}
